59157dcad5b1956de5533a8b19301311e9e691ad
MitchBath <mbath@uncc.edu>
2020-03-19 02:35:54 +0000

    Added a view for the new profile controller as well as a resource for it

--- readyup/config/routes.rb
+++ readyup/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'homepage/index'
+  
+  resources :profile
 
   root 'homepage#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
