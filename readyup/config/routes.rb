c0f1411f34926e1c243794bc6222cf83cf635a21
Cache <ccotie@uncc.edu>
2020-04-20 00:03:54 +0000

    Message

--- readyup/config/routes.rb
+++ readyup/config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destory'
+
+  resources :users
   get 'homepage/index'
   
   resources :profile
-
+  
+  resources :sessions, only: [:new, :create, :destory]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destory', as: 'logout'
   root 'homepage#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
